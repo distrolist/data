1f3b478a960cd0a71bc207a6fcd6efcef3fe8b4c
Ali Rıza KESKİN <parduscix@yandex.ru>
2021-05-20 17:42:23 +0300

    steleton changed

--- update_distro.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-import os
-f=open("main.list","w")
-link="https://raw.githubusercontent.com/distrolist/data/master/distro/"
-for distro in os.listdir("distro"):
-    print(distro)
-    f.write(link+distro)
